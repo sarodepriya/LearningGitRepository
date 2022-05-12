cfbe9dbdc40889efcc6de74d62caefdf72f0f980
sarodepriya <priya.s1011@gmail.com>
2022-05-12 15:33:41 +0530

    App and Employee Commit.

--- JavaProject1-GitDemo/src/main/java/com/priyanka/git/App.java
+++ JavaProject1-GitDemo/src/main/java/com/priyanka/git/App.java
@@ -1,4 +1,5 @@
 package com.priyanka.git;
+import com.priyanka.git.Employee;
 
 /**
  * Hello world!
@@ -9,5 +10,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    	System.out.println("Employee Details!!");
+		Employee e1 = new Employee("Priyanka","Sarode",101,20000);
+		System.out.println(e1.toString());
     }
 }
--- JavaProject1-GitDemo/src/main/java/com/priyanka/git/Employee.java
+++ JavaProject1-GitDemo/src/main/java/com/priyanka/git/Employee.java
@@ -2,10 +2,47 @@ package com.priyanka.git;
 
 public class Employee {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Employee Class.!!");
+	
+	private String firstname;
+	private String lastname;
+	private int id;
+	private long salary;
+	
+	
+	
 
+	public Employee(String firstname, String lastname, int id, long salary) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.id = id;
+		this.salary = salary;
 	}
 
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	public Employee() {	}
 }
+
